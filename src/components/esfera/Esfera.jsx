7db70ee62e84f4db68382c6eb3f85c73c20c10f4
William <victorwccv@gmail.com>
2024-05-02 21:18:17 -0500

    sphere tank

---
+++ src/components/esfera/Esfera.jsx
@@ -0,0 +1,13 @@
+import "./esfera.css";
+const Esfera = () => {
+  return (
+    <div className="esfera-container">
+      <div className="tanque-esfera"></div>
+      <div className="fill"></div>
+      <div className="ancla"></div>
+      <div className="ancla"></div>
+    </div>
+  );
+};
+
+export default Esfera;
--- src/pages/Esfera3105/Esfera3105.jsx
+++ src/pages/Esfera3105/Esfera3105.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Esfera from '../../components/esfera/Esfera';
 
 const Esfera3105 = () => {
   return (
-    <div>Esfera3105</div>
+    <div>Esfera3105
+      <Esfera />
+    </div>
   )
 }
 
