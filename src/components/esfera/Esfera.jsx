455faee467d8e6f3778f1d10e3a1ac374e8f3bbf
William <victorwccv@gmail.com>
2024-05-06 19:07:51 -0500

    Esfera design complete

--- src/components/esfera/Esfera.jsx
+++ src/components/esfera/Esfera.jsx
@@ -2,10 +2,25 @@ import "./esfera.css";
 const Esfera = () => {
   return (
     <div className="esfera-container">
-      <div className="tanque-esfera"></div>
-      <div className="fill"></div>
+      <div className="tanque-esfera">
+        <div className="fill"></div>
+
+        <ul className="metrics">
+          <li>70 gl</li>
+          <li>60 gl</li>
+          <li>50 gl</li>
+          <li>40 gl</li>
+          <li>30 gl</li>
+          <li>20 gl</li>
+          <li>10 gl</li>
+          <li>0 gl</li>
+        </ul>
+      </div>
       <div className="ancla"></div>
-      <div className="ancla"></div>
+      <div className="ancla right"></div>
+      <div className="ancla small"></div>
+      <div className="ancla small right"></div>
+      <div className="base"></div>
     </div>
   );
 };
